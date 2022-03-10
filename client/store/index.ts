719f56cf714c4ae5a7a921f49774410235fc0aaf
Raúl Pavón <raulpavonfp@gmail.com>
2022-03-10 22:35:38 +0100

    frontend - first step creating store

---
+++ client/store/index.ts
@@ -0,0 +1,50 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import { getAccessorType } from 'typed-vuex'
+
+Vue.use(Vuex)
+
+export const State = () => ({})
+
+export default {
+  state: State,
+}
+
+export type RootState = ReturnType<typeof State>
+
+declare module 'vue/types/vue' {
+  interface Vue {
+    $accessor: typeof accessorType
+  }
+}
+
+declare module '@nuxt/types' {
+  interface NuxtAppOptions {
+    $accessor: typeof accessorType
+  }
+}
+
+// ⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇
+export const accessorType = getAccessorType({
+  modules: {
+    // NOTE: Añade tus módulos aquí
+  },
+})
+
+const mutationAccessorType = getAccessorType({
+  modules: {
+    // NOTE: Añade tus mutaciones aquí
+  },
+})
+// ⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆⬆
+
+type AccessorToPointer<T> = { [K in keyof T]-?:
+  T[K] extends (...args: infer P) => any ? { mutation: K, payload: P } :
+    AccessorToPointer<T[K]> extends infer X ? (
+      X extends { mutation: infer M, payload: infer P } ? (
+        { mutation: `${Extract<K, string>}/${Extract<M, string>}`; payload: P }
+        ) : never
+      ) : never
+}[keyof T]
+
+export type MutationCallPointer = AccessorToPointer<typeof mutationAccessorType>
