1440766c69293aeeace6af03ff283b4d6249d58e
Raúl Pavón <raulpavonfp@gmail.com>
2022-03-10 22:38:26 +0100

    frontend - changed references from Models

--- client/Modules/Auth/Domain/Service/AuthServiceInterface.ts
+++ client/Modules/Auth/Domain/Service/AuthServiceInterface.ts
@@ -1,5 +1,5 @@
 import { UserId } from '~/Modules/Auth/Domain/ValueObjects/UserId'
-import { User } from '~/Modules/Auth/Domain/Modules/User'
+import { User } from '~/Modules/Auth/Domain/Models/User'
 
 export interface AuthServiceInterface {
   fetchUser: (userId: UserId) => Promise<User|undefined>
