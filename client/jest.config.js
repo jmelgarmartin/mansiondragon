48df41fd9ced0664967297dff7d953e2619a32f2
Raúl Pavón <raulpavonfp@gmail.com>
2022-03-11 19:33:14 +0100

    frontend - removed the collectCoverage option

--- client/jest.config.js
+++ client/jest.config.js
@@ -15,7 +15,7 @@ module.exports = {
     '^.+\\.js$': 'babel-jest',
     '.*\\.(vue)$': 'vue-jest',
   },
-  collectCoverage: true,
+  collectCoverage: false,
   collectCoverageFrom: [
     '<rootDir>/Modules/**/*.vue',
     '<rootDir>/Modules/**/*.ts',
