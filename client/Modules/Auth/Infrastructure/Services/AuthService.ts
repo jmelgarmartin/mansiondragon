253e0d98df0998703b3e4c441221aa5b7195c9ba
Raúl Pavón <raulpavonfp@gmail.com>
2022-03-11 19:25:35 +0100

    frontend - added first test to AuthService

--- client/Modules/Auth/Infrastructure/Services/AuthService.ts
+++ client/Modules/Auth/Infrastructure/Services/AuthService.ts
@@ -1,16 +1,30 @@
 import { AxiosInstance } from 'axios'
 import { AuthServiceInterface } from '~/Modules/Auth/Domain/Service/AuthServiceInterface'
 import { User, Id as UserId } from '~/Modules/Auth/Domain/Models/User'
+import { WrapperApiResponse } from '~/Modules/Shared/Infrastructure/DTOs/WrapperApiResponse'
+import { FetchUserApiResponse } from '~/Modules/Auth/Infrastructure/DTOs/FetchUserApiResponse'
 
 export class AuthService implements AuthServiceInterface {
   public constructor (private lamansionApi: AxiosInstance) {}
 
   public async fetchUser (userId: UserId): Promise<User|undefined> {
     const endpoint = `auth/user/${userId}`
-    const response = await this.lamansionApi.get(endpoint)
+    const response = await this.lamansionApi.get<WrapperApiResponse<FetchUserApiResponse>>(endpoint)
 
-    console.log(response.data)
+    return response.data.data.map(this.toUser)[0]
+  }
 
-    return Promise.resolve(User.make({ id: userId, name: 'a' }))
+  private toUser (requestedUser: FetchUserApiResponse): User {
+    const {
+      id,
+      attributes: {
+        username,
+        player,
+        master,
+        admin,
+      },
+    } = requestedUser
+
+    return User.make({ id, name: username, master, admin, player })
   }
 }
--- client/tests/Modules/Auth/Infrastructure/Services/AuthService.spec.ts
+++ client/tests/Modules/Auth/Infrastructure/Services/AuthService.spec.ts
@@ -1,3 +1,48 @@
-describe('', () => {
-  
-});
+import { AxiosInstance } from 'axios'
+import { AuthServiceInterface } from '~/Modules/Auth/Domain/Service/AuthServiceInterface'
+import { AuthService } from '~/Modules/Auth/Infrastructure/Services/AuthService'
+import { WrapperApiResponse } from '~/Modules/Shared/Infrastructure/DTOs/WrapperApiResponse'
+import { FetchUserApiResponse } from '~/Modules/Auth/Infrastructure/DTOs/FetchUserApiResponse'
+import mocked = jest.mocked;
+
+describe('client/Modules/Auth/Infrastructure/Services/AuthService.ts', () => {
+  let lamansionApi: AxiosInstance
+
+  beforeEach(() => {
+    lamansionApi = {
+      get: jest.fn().mockRejectedValue(new Error('unexpected call to axios.get')),
+    } as unknown as AxiosInstance
+  })
+
+  describe('fetchUser', () => {
+    let authService: AuthServiceInterface
+
+    beforeEach(() => {
+      authService = new AuthService(lamansionApi)
+
+      const fetchUser: WrapperApiResponse<FetchUserApiResponse> = {
+        data: [{
+          id: 'a-user-id',
+          type: 'user',
+          attributes: {
+            username: 'a-user-name',
+            admin: true,
+            image: 'a-user-image',
+            master: false,
+            player: true,
+          },
+        }],
+      }
+
+      mocked(lamansionApi.get).mockResolvedValue({
+        data: fetchUser,
+      })
+    })
+
+    it('should call the endpoint url correctly', async () => {
+      await authService.fetchUser('discord-user-id')
+
+      expect(lamansionApi.get).toHaveBeenCalledWith('auth/user/discord-user-id')
+    })
+  })
+})
