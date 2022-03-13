8d2284b3296bc9c37e02f9ddf282e6b3d82547fd
Raúl Pavón <raulpavonfp@gmail.com>
2022-03-13 17:02:11 +0100

    backend - installed typeorm and added configuration

--- server/src/Modules/App/Infrastructure/Module/app.module.ts
+++ server/src/Modules/App/Infrastructure/Module/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from '~/src/Modules/Auth/Infrastructure/Module/AuthModule';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [AuthModule, ConfigModule.forRoot()],
+  imports: [
+    AuthModule,
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot(),
+  ],
   controllers: [],
   providers: [],
 })
