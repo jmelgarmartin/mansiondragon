8c619f735c42d7f9d7c5f8812e6fba439792cdbf
Raúl Pavón <raulpavonfp@gmail.com>
2022-03-18 20:20:22 +0100

    backend - added return type

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './Modules/App/Infrastructure/Module/app.module';
 
-async function bootstrap() {
+async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('/api');
