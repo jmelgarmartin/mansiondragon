300ba19cebe526bb9da7f4f9fb2917f204a70970
Raúl Pavón <raulpavonfp@gmail.com>
2022-03-18 19:01:04 +0100

    backend - updated module with Repository and usecase

--- server/src/Modules/Auth/Infrastructure/Module/AuthModule.ts
+++ server/src/Modules/Auth/Infrastructure/Module/AuthModule.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from '~/src/Modules/Auth/Infrastructure/Controllers/Auth';
+import { UserFetcher } from "~/src/Modules/Auth/Application/UseCases/UserFetcher";
+import { UserRepository } from "~/src/Modules/Auth/Infrastructure/Repositories/UserRepository";
 
 @Module({
   imports: [],
   controllers: [AuthController],
-  providers: [],
+  providers: [UserFetcher, UserRepository],
 })
 export class AuthModule {}
