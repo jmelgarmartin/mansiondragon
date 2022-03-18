6d6207c424e3cf9d4adcb1ac741944651883793b
Raúl Pavón <raulpavonfp@gmail.com>
2022-03-18 19:29:22 +0100

    backend - added dependencies to module

--- server/src/Modules/Auth/Infrastructure/Module/AuthModule.ts
+++ server/src/Modules/Auth/Infrastructure/Module/AuthModule.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { FetchUserController } from '~/src/Modules/Auth/Infrastructure/Controllers/FetchUser';
 import { UserFetcher } from "~/src/Modules/Auth/Application/UseCases/UserFetcher";
 import { UserRepository } from "~/src/Modules/Auth/Infrastructure/Repositories/UserRepository";
+import { CreateUserController } from "~/src/Modules/Auth/Infrastructure/Controllers/CreateUser";
+import { UserCreator } from "~/src/Modules/Auth/Application/UseCases/UserCreator";
 
 @Module({
   imports: [],
-  controllers: [ FetchUserController ],
-  providers: [ UserFetcher, UserRepository ],
+  controllers: [ FetchUserController, CreateUserController ],
+  providers: [ UserFetcher, UserCreator, UserRepository ],
 })
 export class AuthModule {}
