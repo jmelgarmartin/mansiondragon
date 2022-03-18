39c1add68e18d78d6bb70676b260901b2a1ad0fc
Raúl Pavón <raulpavonfp@gmail.com>
2022-03-18 19:01:22 +0100

    backend - finished fetchDiscordUser endpoint

--- server/src/Modules/Auth/Infrastructure/Controllers/Auth.ts
+++ server/src/Modules/Auth/Infrastructure/Controllers/Auth.ts
@@ -1,13 +1,40 @@
 import { Controller, Get, Param, Req } from '@nestjs/common';
+import {UserFetcher} from "~/src/Modules/Auth/Application/UseCases/UserFetcher";
+import {User} from "~/src/Modules/Auth/Domain/Models/User";
+import { Response, response } from "express";
+import {GenericApiResponse, GenericModelResponse} from "~/src/Modules/Shared/Infrastructure/DTOs/GenericApiResponse";
+import {UserApiResponse} from "~/src/Modules/Auth/Infrastructure/DTOs/UserApiResponse";
 
 @Controller('auth')
 export class AuthController {
+  constructor(private userFetcher: UserFetcher) {
+  }
+
   @Get('/discord/user/:id')
-  fetchUser(@Param('id') id: string) {
-    return [
-      {
-        message: `Hello there! ${id}`,
-      },
-    ];
+  async fetchDiscordUser(@Param('id') id: string):Promise<GenericApiResponse<UserApiResponse>> {
+    const user: User = await this.userFetcher.fetchByDiscordId(id)
+
+    return ({
+      data: [
+        {
+          type: 'user',
+          id: user.id,
+          attributes: {
+            name: user.name
+          },
+          relationships: {
+            roles: {
+              data: user.roles.length > 0 ? user.roles.map((role) => ({
+                type: 'role',
+                id: role.id,
+                attributes: {
+                  name: role.name
+                }
+              })) : null
+            }
+          }
+        }
+      ]
+    })
   }
 }
