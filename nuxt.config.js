23dcf0a33bdb258970fb26b7d8d63aa8158c10b9
JESUS MELGAR MARTIN <jmelgar@atsistemas.com>
2022-03-22 13:31:55 +0100

    prueba

---
+++ nuxt.config.js
@@ -0,0 +1,76 @@
+export default {
+  // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
+  ssr: false,
+  srcDir: 'client/',
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'lamansiondeldragon',
+    htmlAttrs: {
+      lang: 'es',
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' },
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+    { src: './Modules/Home/Infrastructure/Bindings' },
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: false,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    // https://go.nuxtjs.dev/typescript
+    '@nuxt/typescript-build',
+    // https://go.nuxtjs.dev/tailwindcss
+    '@nuxtjs/tailwindcss',
+    [
+      '@nuxtjs/router',
+      {
+        path: './client/Modules/Router/Infrastructure',
+        fileName: 'CreateRouter.ts',
+        keepDefaultRouter: false,
+        parsePages: false,
+      },
+    ],
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    // https://go.nuxtjs.dev/axios
+    '@nuxtjs/axios',
+    // https://go.nuxtjs.dev/pwa
+    '@nuxtjs/pwa',
+    // https://github.com/nuxt-community/dotenv-module
+    '@nuxtjs/dotenv',
+    // https://github.com/microcipcip/cookie-universal/tree/master/packages/cookie-universal-nuxt#readme
+    'cookie-universal-nuxt',
+  ],
+
+  // PWA module configuration: https://go.nuxtjs.dev/pwa
+  pwa: {
+    manifest: {
+      lang: 'es',
+    },
+  },
+
+  // Axios module configuration: https://go.nuxtjs.dev/config-axios
+  axios: {
+  },
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {},
+  env: {
+    baseUrl: process.env.API_URL,
+  },
+}
